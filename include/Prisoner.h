32d38db4db0527f7a0fff5f8aea9e4751ee91d4d
João Paulo <127864968+zHawkZ@users.noreply.github.com>
2023-04-08 20:35:00 -0300

    Update Prisoner.h

--- include/Prisoner.h
+++ include/Prisoner.h
@@ -1,25 +1,26 @@
-#ifndef PRISONER_H
-#define PRISONER_H
+#ifndef Prisoner_H
+#define Prisoner_H
 
+#pragma once
 #include "Person.h"
 
 class Prisoner  : public Person
 {
     public:
+       
         Prisoner();
-
         Prisoner(string);
-
         Prisoner(string name, string cpf, string skinColor, char sex, int age, bool pdl);
-
+        
+        
+        //Crime
         void setCrime(string);
-
         string getCrime();
-
+        
         virtual ~Detento();
 
-    private:
+    protected:
         string crime;
 };
 
-#endif // DETENTO_H
+#endif 
