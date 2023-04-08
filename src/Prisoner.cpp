ee64fb15ba157498a2c6cb5216eab5031b9a4d83
João Paulo <127864968+zHawkZ@users.noreply.github.com>
2023-04-08 20:44:52 -0300

    Create Prisoner.cpp

---
+++ src/Prisoner.cpp
@@ -0,0 +1,22 @@
+#include "Prisoner.h"
+
+//Construtores
+Prisoner::Prisoner(): Person(), crime(desconhecido){}
+Prisoner::Prisoner(string crime){
+  setCrime(crime);
+}
+Prisoner::Prisoner(string name, string cpf, string skinColor, char sex, int age, bool pdl, string crime): Person(name, cpf, skinColor, sex, age, pdl){
+  Prisoner(crime);
+ }
+        
+void Prisoner::setCrime(string crime){
+  if(crime == "")
+        this->crime = desconhecido;
+    else
+        this->crime = crime;
+}
+string Prisoner::getCrime(){
+  return crime;
+}
+        
+virtual ~Detento(){}
